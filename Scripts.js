87ad7e803e98287ab39b90c14e4b5f116ddb907a
Kerouline-Ariane-Da-Silva <kerouline.silva@aluno.fmpsc.edu.br>
2024-09-06 11:10:57 -0300

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -2,7 +2,9 @@ document.getElementById('messageButton').addEventListener('click',function(){
     var messageParagraph = document.getElementById('message');
     if (messageParagraph.classList.contains('hidden')) {
         messageParagraph.classList.remove('hidden');
+       imagemSetas.classList.remove('hidden');
     }else{
         messageParagraph.classList.add('hidden');
+        imagemSetas.classList.add('hidden');
     }
 });
